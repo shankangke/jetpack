957e168dde701262080f3711773c42d3c428299e
Christian Gastrell <cgastrell@gmail.com>
2025-09-22 13:01:45 -0300

    add layout support on block registration and new shared (editor+frontend) stylesheet

--- projects/packages/forms/src/blocks/contact-form/index.js
+++ projects/packages/forms/src/blocks/contact-form/index.js
@@ -57,6 +57,17 @@ export const settings = {
 			margin: true,
 		},
 		align: [ 'wide', 'full' ],
+		layout: {
+			default: {
+				type: 'flex',
+				flexWrap: 'wrap',
+			},
+			allowSwitching: false,
+			allowEditing: true,
+			allowOrientation: true,
+			allowVerticalAlignment: false,
+			allowJustification: false,
+		},
 	},
 	attributes: defaultAttributes,
 	providesContext: {

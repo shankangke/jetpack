7e3b1e7c7d384d42fe1f22459691b1ef592ff363
Christian Gastrell <cgastrell@gmail.com>
2025-11-17 11:54:39 -0300

    move new css file to jetpack-forms-layout for better organization

--- projects/packages/forms/src/blocks/contact-form/class-contact-form-block.php
+++ projects/packages/forms/src/blocks/contact-form/class-contact-form-block.php
@@ -55,7 +55,7 @@ class Contact_Form_Block {
 						'allowJustification'     => false,
 					),
 				),
-				'style_handles'   => array( 'jetpack-contact-form' ),
+				'style_handles'   => array( 'jetpack-forms-layout' ),
 			)
 		);
 

0bf338c62a764996fe8983e4ee27c0e31a421f99
annacmc <anna.mcphee@automattic.com>
2025-12-04 09:38:42 +1100

    Add CategoryBar component exports

---
+++ projects/js-packages/charts/src/components/category-bar/index.ts
@@ -0,0 +1,9 @@
+export { default as CategoryBar, CategoryBarUnresponsive } from './category-bar';
+export type { CategoryBarResponsiveConfig } from './category-bar';
+
+export type {
+	CategoryBarProps,
+	CategoryBarSegment,
+	CategoryBarMarker,
+	CategoryBarMode,
+} from './types';
